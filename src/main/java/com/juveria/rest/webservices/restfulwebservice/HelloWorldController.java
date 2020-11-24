b17365c7d52c11b4c32c27e76ffec63cbed7987e
Juveria Manzar <juveriamanzar29@gmail.com>
2020-11-25 05:01:02 +0530

    initial-commit

---
+++ src/main/java/com/juveria/rest/webservices/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.juveria.rest.webservices.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	//GET
+	//URI-/hello-world
+	//method-"Hello World"
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
