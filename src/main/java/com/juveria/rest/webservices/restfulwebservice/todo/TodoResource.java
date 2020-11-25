9939023fc3cbce80a11f0bcd3efe697cfb6efa3c
Juveria Manzar <juveriamanzar29@gmail.com>
2020-11-26 03:27:54 +0530

    delete by id service

--- src/main/java/com/juveria/rest/webservices/restfulwebservice/todo/TodoResource.java
+++ src/main/java/com/juveria/rest/webservices/restfulwebservice/todo/TodoResource.java
@@ -1,6 +1,9 @@
 package com.juveria.rest.webservices.restfulwebservice.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class TodoResource {
 	
 	@Autowired
@@ -17,4 +21,14 @@ public class TodoResource {
 	public List<Todo> getAllTodos(@PathVariable String username){
 		return todoService.findAll();
 	}
+	
+	@DeleteMapping("users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+		Todo todo= todoService.deleteById(id);
+		if(todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 }
