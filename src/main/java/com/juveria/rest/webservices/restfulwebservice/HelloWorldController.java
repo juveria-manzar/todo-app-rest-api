a2dba5eab1baead33ec4e67e096d4e0fc518c775
Juveria Manzar <juveriamanzar29@gmail.com>
2020-11-25 22:15:19 +0530

    cors-origin-resolution

--- src/main/java/com/juveria/rest/webservices/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/juveria/rest/webservices/restfulwebservice/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 	//GET
 	//URI-/hello-world
